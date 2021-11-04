82014a77408718e2870ec425cbe9a433341be1f4
julia-rolemberg <61888090+julia-rolemberg@users.noreply.github.com>
2021-11-04 17:05:44 -0300

    Login Test

--- LoginTest.py
+++ LoginTest.py
@@ -9,16 +9,23 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 driver.get('http://localhost:1337/')
 
-
 time.sleep(5)
-
 
 iconeLogin = WebDriverWait(driver, 20).until(
 	EC.presence_of_element_located((By.ID, 'iconeUsuario'))
 )
 iconeLogin.click()
 
+inputEmail = WebDriverWait(driver, 20).until(
+	EC.presence_of_element_located((By.ID, 'email'))
+)
+inputSenha = WebDriverWait(driver, 20).until(
+	EC.presence_of_element_located((By.ID, 'senha'))
+)
 
+inputEmail.send_keys('admin')
+inputSenha.send_keys('1234')
+inputEmail.send_keys(Keys.RETURN)
 
 
 time.sleep(5)
